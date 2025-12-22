aab17f49d9191c27adfbeb72a1ff7ca545f7ad53
Jaewon Kim <130392151+bucheoncityboy@users.noreply.github.com>
2025-12-22 14:14:21 +0900

    Update main.py

--- ob_strategy/main.py
+++ ob_strategy/main.py
@@ -1,24 +1,23 @@
-# main.py
-# -*- coding: utf-8 -*-
-
+import asyncio
 import logging
 from config import API_KEY, API_SECRET, DEFAULT_CONFIG
 from bot import TradingBot
 
-def main():
-    # 로깅 설정
+async def main():
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
+        format='%(asctime)s - [ASYNC] - %(levelname)s - %(message)s',
         handlers=[logging.StreamHandler()]
     )
 
     try:
-        # 설정과 키를 사용하여 봇 인스턴스 생성
         bot = TradingBot(API_KEY, API_SECRET, DEFAULT_CONFIG)
-        bot.run()
+        await bot.run_async()
     except Exception as e:
-        logging.critical(f"봇 초기화 또는 실행 중 심각한 오류 발생: {e}", exc_info=True)
+        logging.critical(f"Fatal Error: {e}", exc_info=True)
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
